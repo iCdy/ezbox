3c4b5cbdbc6e85b3ea79b053aeeff25faa4980c8
mengran chi <mengranchi@emails.bjut.edu.cn>
2025-06-04 23:48:04 +0800

    update ezproxy and ezvllm

--- init.sh
+++ init.sh
@@ -8,20 +8,15 @@ if [ $? -ne 0 ]; then
   exit 1
 fi
 
-# 检查 .bashrc 文件中是否已添加环境变量
 if ! grep -q "export PATH=\$HOME/bin:\$PATH" "$HOME/.bashrc"; then
-  # 添加环境变量到 .bashrc
   echo "export PATH=\$HOME/bin:\$PATH" >> $HOME/.bashrc
   echo "已添加环境变量到 .bashrc"
 else
   echo ".bashrc 中已存在环境变量设置，跳过"
 fi
 
-# 检查 .zshrc 文件是否存在
 if [ -f "$HOME/.zshrc" ]; then
-  # 检查 .zshrc 文件中是否已添加环境变量
   if ! grep -q "export PATH=\$HOME/bin:\$PATH" "$HOME/.zshrc"; then
-    # 添加环境变量到 .zshrc
     echo "export PATH=\$HOME/bin:\$PATH" >> $HOME/.zshrc
     echo "已添加环境变量到 .zshrc"
   else
@@ -39,12 +34,8 @@ cp  ./ezproxy $HOME/bin
 chmod +x $HOME/bin/ezvllm
 chmod +x $HOME/bin/ezproxy
 
-# 输出提示信息
 echo "ezvllm 已安装到 $HOME/bin 目录。"
 echo "ezproxy 已安装到 $HOME/bin 目录。"
-# 提示用户重新加载配置
 echo "请运行 'source ~/.bashrc' 或 'source ~/.zshrc' 以使更改生效。"
-# 提示用户使用 ezvllm 命令
-echo "您可以使用 'ezvllm' 命令来运行 ezvllm。"
 
 
